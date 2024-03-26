987d3225a4a15a39e7f5e9b7fd90c4e01a25b308
tzz-zhao <tzz.zhao@gmail.com>
2024-03-26 16:01:19 +0900

    Layout追加

---
+++ src/config/site.config.ts
@@ -0,0 +1,6 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+    name: 'Tzz.Dev',
+    description: '日々の開発メモ、たまにはプライベートのことも。'
+}
---
+++ src/types/index.d.ts
@@ -0,0 +1,4 @@
+export type SiteConfig = {
+    name?: string,
+    description?: string,
+}
