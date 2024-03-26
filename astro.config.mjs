d96de27877a774bc9da109d05c71c0f9be100b43
tzz-zhao <tzz.zhao@gmail.com>
2024-03-26 11:52:50 +0900

    sitemap導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tzz.dev',
   integrations: [
     react(),
+    sitemap(),
     tailwind({
       applyBaseStyles: false,
     }),
