acf57ab06782415646cbc5856263efa7155e4e26
tzz-zhao <tzz.zhao@gmail.com>
2024-03-27 10:24:29 +0900

    ドメイン対応削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,17 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tzz.dev',
-  output: 'server',
-  adapter: vercel(),
   i18n: {
     defaultLocale: 'ja',
     locales: ['ja', 'en', 'zh'],
-    routing: {
-      prefixDefaultLocale: false
-    },
     fallback: {
       en: 'ja',
       zh: 'ja'
     },
-    domains: {
-      en: 'https://en.tzz.dev',
-      zh: 'https://zh.tzz.dev'
-    },
-  },
-  experimental: {
-    i18nDomains: true
   },
   integrations: [react(), sitemap(), tailwind({
     applyBaseStyles: false
