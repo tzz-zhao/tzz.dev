673e6cba465d923cb50eaaeed0f7d867661c2ce4
tzzjp <tzz.zhao@gmail.com>
2025-02-03 23:46:16 +0900

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,20 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://tzz.dev",
+  i18n: {
+    locales: ["ja", "zh-CN"],
+    defaultLocale: "ja",
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
+});
